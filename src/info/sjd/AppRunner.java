b38414bc1aef88234a8663a94d7ad75c49044152
vitaliy-tyma <vitaliy-tyma@DESKTOP-OBMD4J8>
2018-09-26 06:41:50 +0300

    Methods added, MIN values are introduced. Tests are to be completed.

--- src/info/sjd/AppRunner.java
+++ src/info/sjd/AppRunner.java
@@ -2,8 +2,29 @@ package info.sjd;
 
 public class AppRunner {
 
+	public static int sumOfAllDigits1(int num) {
+		int rest = 0;
+		int result = 0;
 
+		while (num > 0) {
+			rest = num%10;			
+			result += rest;
+			num /= 10;
+		}
+		return result;
+	}
 	
+
+	public static int sumOfAllDigits2(int num) {
+		String num_string = Integer.toString(num);
+		int result = 0;
+		for (int i=0; i < num_string.length(); i++) {
+			result += Character.getNumericValue(num_string.charAt(i));
+		}
+		return result;
+	}
+
+		
 	public static boolean isPalindrome(int cand) {
 		String str = Integer.toString(cand); 
 		int len = str.length();
@@ -16,9 +37,35 @@ public class AppRunner {
 		return true;
 	}	
 	
+	public static int maxPalindrome(int NUM1_MIN, int NUM1_MAX,
+									int NUM2_MAX, int NUM2_MAX) {
+		
+		int candidate = 0;
+		int max_palindrome = NUM1_MIN * NUM2_MIN; //Start number;
+
+		
+		for (int j = NUM1_MIN; j <= NUM1_MAX; j++) {
+			for (int k = NUM2_MIN; k <= NUM2_MAX; k++){
+				candidate = j*k;
+				
+				if (isPalindrome(candidate) == true){
+					if (candidate > max_palindrome) {
+						max_palindrome = candidate;
+					}
+				}
+				
+			}
+		}
+		return max_palindrome;
+	}
 	
 	
+/*********************************************************/	
 	public static void main(String[] args) {
+		
+		private static Logger logger = Logger.getLogger(
+				AppRunner.class.getName());
+		
 		/* Task 01
 		 * Sum of all digits in 12345
 		 * 1) module
@@ -26,38 +73,26 @@ public class AppRunner {
 		 * */
 		
 		int num = 12345;
-		String num_string = Integer.toString(num);
-		
-		int rest = 0;
 		int result = 0;
 		
 		
 		
 		
-		/*Solution 1*/
-		while (num > 0) {
-			
-			rest = num%10;
-			
-			result += rest;
-			
-			num /= 10;
-			
-
-		}
 		
-		System.out.println("Sum (Solution 1) = "+result);
+		/*Solution 1*/
+		result = sumOfAllDigits1(num);		
+		logger.info("Sum (Solution 1) = " + result);
+//		System.out.println("Sum (Solution 1) = "+result);
+		
 
-
+		
+		result = 0;
 		
 		
 		/*Solution 2*/
-		result = 0;
-		
-		for (int i=0; i < num_string.length(); i++) {
-			result += Character.getNumericValue(num_string.charAt(i));
-		}
-		System.out.println("Sum (Solution 2) = "+result);
+		result = sumOfAllDigits2(num);
+		logger.info("Sum (Solution 2) = " + result);
+//		System.out.println("Sum (Solution 2) = "+result);
 		/* END OF TASK 1*/		
 		
 		
@@ -71,24 +106,19 @@ public class AppRunner {
 		 * */		
 		final int NUM1_MAX = 9999;
 		final int NUM2_MAX = 9999;
-		int candidate = 0;
-		int max_palindrome = 0; //First palindrome
+		final int NUM1_MIN = 1000;
+		final int NUM2_MIN = 1000;
+
+		int max_palindrome = 0;
+		max_palindrome = maxPalindrome(NUM1_MIN,NUM1_MAX,NUM2_MAX,NUM2_MAX);
 		
-		for (int j = 0; j <= NUM1_MAX; j++) {
-			for (int k = 0; k <= NUM2_MAX; k++){
-				candidate = j*k;
-				
-				if (isPalindrome(candidate) == true){
-					if (candidate > max_palindrome) {
-						max_palindrome = candidate;
-					}
-				}
-				
-			}
-		}
-		System.out.println(
-				"Max palindrome of two 4-rank numbers multiplication  = "
+		logger.info(
+				"Max palindrome of two 4-rank numbers multiplication  = " + 
 				+ max_palindrome);
+/*		System.out.println(
+		"Max palindrome of two 4-rank numbers multiplication  = "
+		+ max_palindrome);
+*/	
 		/* END OF TASK 2*/		
 
 		
@@ -103,5 +133,3 @@ public class AppRunner {
 	
 
 }
-
-
